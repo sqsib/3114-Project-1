74153357090a622f6b30c530ed5499df53cff0a7
Sib Quayum <sqsib94@vt.edu>
2017-09-13 10:39:05 -0400

    second commit

--- src/BST.java
+++ src/BST.java
@@ -35,11 +35,45 @@ public class BST<T extends Comparable<? super T>> {
 
 	public T find( T x ) { 
 
-		return x;
+		return findHelper(x, root);
+	}
+
+	private T findHelper(T x, BinaryNode sRoot) {
+		if ( sRoot == null )
+			return null;
+		int compareResult = x.compareTo( sRoot.element );
+
+		if ( compareResult < 0 )
+			return findHelper( x, sRoot.left );
+		else if ( compareResult > 0 )
+			return findHelper( x, sRoot.right );
+		else
+			return sRoot.element; // Match
+
 	}
 	public boolean insert( T x ) { 
 
-		return false;
+		T temp = find(x);
+		if(temp == null) {
+			root = insertHelper(x, root);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	private BinaryNode insertHelper(T x, BinaryNode sRoot) {
+		if ( sRoot == null )
+			return new BinaryNode( x, null, null );
+		int compareResult = x.compareTo( sRoot.element );
+		if ( compareResult < 0 )
+			sRoot.left = insertHelper( x, sRoot.left );
+		else if ( compareResult > 0 )
+			sRoot.right = insertHelper( x, sRoot.right );
+		else
+			;
+		return sRoot;
 	}
 
 	public boolean remove( T x ) { 
@@ -50,7 +84,53 @@ public class BST<T extends Comparable<? super T>> {
 	}
 
 	public boolean equals(Object other) { 
-		return false;
+		if(other == null) {
+			return false;
+		}
+
+		if(!this.getClass().equals(other.getClass())) {
+			return false;
+		}
+
+		BST<T> obj = (BST<T>)other;
+		BinaryNode otherRoot = obj.root;
+		return equalsHelper(root, otherRoot);
+	}
+
+	//Binary Search Trees are equal if the roots are equal
+	// and the roots' children are equal, each child can be 
+	// a root
+	public boolean equalsHelper(BinaryNode n1, BinaryNode n2) {
+		//check if n1 and n2 are the same thing
+		if(n1 == n2) {
+			return true;
+		}
+		//if either of the roots are null return false
+		if(n1 == null) {
+			return false;
+		}
+		if(n2 == null) {
+			return false;
+		}
+
+		// check actual data of the root, then 
+		// recursively check the left and right chlild
+		// as they are the root of their own 
+		// respective subtree
+		if(!(n1.element.equals(n2.element))) {
+			return false;
+		}
+		else {
+			if((equalsHelper(n1.right, n2.right) && equalsHelper(n1.left, n2.left))) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+
+
 	}
 	// private methods follow
 
--- src/Rectangle1.java
+++ src/Rectangle1.java
@@ -3,6 +3,14 @@ public class Rectangle1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		BST<Integer> test = new BST<Integer>();
+		
+		boolean t = test.insert(5);
+		System.out.println(t);
+		Integer toto = test.find(5);
+		System.out.println(toto);
+		
 
 	}
 
